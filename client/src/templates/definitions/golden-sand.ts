2b5a5cae810cc8ba5b6b79f25ca80175a9e551fd
AnthonyRdutra <anthonyoriebir@gmail.com>
2025-12-23 10:52:29 -0300

    gif support

---
+++ client/src/templates/definitions/golden-sand.ts
@@ -0,0 +1,11 @@
+import {  CardTemplateDefinition } from "../types";
+
+export const GoldenSand:  CardTemplateDefinition = {
+  name: "Golden Sand",
+  description: "Animated golden particles with premium glass",
+  className: "golden-sand-effect", // <--- TEM QUE SER IGUAL AO CSS (.golden-sand-effect)
+  accentColor: "#ffd700",
+  animation: "lights", // Use "lights" ou "stars"
+  preview: "⏳", 
+  textColor: "#f0f0f0"
+};
